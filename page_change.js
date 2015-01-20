09f8b03bfe01ea93b6ceebe3ed0d68057051ae8a
Robert Rice <robertrice@Roberts-MacBook-Pro.local>
2015-01-20 11:17:30 -0500

    typo

--- page_change.js
+++ page_change.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    function set_new_destin(jqobjecct, val, destin) {
+    function set_new_destin(jqobject, val, destin) {
       //jqobject = jquery object
       //val = value of object to trigger page change
       //destin =  new destination page
